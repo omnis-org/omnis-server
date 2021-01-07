9b0f7985b3a225e041967fdbd9a5af195899455b
Arthur Guyader <arthur@guyader.info>
2021-01-07 20:33:18 +0100

    Add documentation for utils package
    Signed-off-by: Arthur Guyader <arthur@guyader.info>
    Change-Id: I6cf13b7a5bdc9337d6cd569d568c213cc3d50355

--- internal/utils/ip.go
+++ internal/utils/ip.go
@@ -6,7 +6,10 @@ import (
 	"github.com/brotherpowers/ipsubnet"
 )
 
-// GetNetworkPart should have a comment.
+// GetNetworkPart allows to get the network subnet of an
+// ip address : 192.168.1.1/24 => 192.168.1.0
+// if invalid ip or mask <= 0 return "0.0.0.0"
+// if mask >= 32 return ip
 func GetNetworkPart(ip string, mask int) string {
 	if net.ParseIP(ip) == nil || mask <= 0 {
 		return "0.0.0.0"
